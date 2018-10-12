821bcfa1b01b06ecc892a05065647d349fb3a0da
Ethan Koenig <ethantkoenig@gmail.com>
2018-10-12 07:55:32 -0700

    Randomly choose activity in nag.py

--- bin/nag.py
+++ bin/nag.py
@@ -2,11 +2,21 @@
 
 import os
 import pickle
+import random
+import sys
 import subprocess
 import time
 
-with open(os.path.join(os.getenv('HOME'), '.last_break.pickle'), 'rb') as f:
-    last_break = pickle.load(f)
+state_filename = os.path.join(os.getenv('HOME'), '.nag_state.pickle')
 
-if time.time() > last_break + 1800:
-    subprocess.run(['notify-send', '--urgency=critical', 'Time for a break!'])
+with open(state_filename, 'rb') as f:
+    last_break, current_task = pickle.load(f)
+
+if current_task is None:
+    if time.time() < last_break + 1500:
+        sys.exit(0)
+    current_task = random.choice(['5-minute break', 'Hamstring stretch', 'Hand stretches'])
+    with open(state_filename, 'wb') as f:
+        pickle.dump((last_break, current_task), f)
+
+subprocess.run(['notify-send', '--urgency=critical', current_task])
--- bin/update_last_break.py
+++ bin/update_last_break.py
@@ -4,5 +4,7 @@ import os
 import pickle
 import time
 
-with open(os.path.join(os.getenv('HOME'), '.last_break.pickle'), 'wb') as f:
-    pickle.dump(time.time(), f)
+state_filename = os.path.join(os.getenv('HOME'), '.nag_state.pickle')
+
+with open(state_filename, 'wb') as f:
+    pickle.dump((time.time(), None), f)
